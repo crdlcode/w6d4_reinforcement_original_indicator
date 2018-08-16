93d859573acf2a99a47062f8ce8c64147b5a4852
curtischatch <curtis.chat.hk@gmail.com>
2018-08-15 21:40:06 -0400

    og_i method works

---
+++ exercise.rb
@@ -0,0 +1,21 @@
+def og_i(num)
+  if num.digits.first == 1
+    "#{(num)}st"
+  elsif num.digits.first == 2
+    "#{(num)}nd"
+  elsif num.digits.first == 3
+    "#{(num)}rd"
+  else
+    "#{(num)}th"
+  end
+end
+
+p og_i(1)
+p og_i(2)
+p og_i(3)
+p og_i(10)
+p og_i(32424)
+p og_i(413232)
+p og_i(2435433)
+p og_i(4335)
+p og_i(23421)
